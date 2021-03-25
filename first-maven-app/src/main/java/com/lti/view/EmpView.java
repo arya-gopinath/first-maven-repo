65f2efcf4474c41de54bda788ca9194a3b12578a
Arya <aryagopinath1998@gmail.com>
2021-03-25 12:45:08 +0530

    commit

--- first-maven-app/src/main/java/com/lti/view/EmpView.java
+++ first-maven-app/src/main/java/com/lti/view/EmpView.java
@@ -5,8 +5,8 @@ import com.lti.model.Employee;
 public class EmpView {
 	public static void main(String[] args){
 		Employee emp=new Employee();
-		emp.setEmpId(1003);
-		emp.setEmpName("Kevin");
+		emp.setEmpId(1004);
+		emp.setEmpName("Mike");
 		
 		System.out.println("Id:" +emp.getEmpId());
 		System.out.println("Name: "+emp.getEmpName());
